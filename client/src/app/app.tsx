34fa03c3e1de65c404e48c08c964d89c4ea29407
alexmochu <mochualex4@gmail.com>
2023-04-12 09:38:16 +0300

    setup axios on client

--- client/src/app/app.tsx
+++ client/src/app/app.tsx
@@ -1,12 +1,36 @@
+import * as React from 'react';
+import { useCallback, useEffect, useState } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import axios from 'axios';
 
 import Fantium from './fantium';
 
 export function App() {
+const [data, setData] = useState('data')
+  const getFetchUrl = useCallback(
+    () =>
+      `http://localhost:3333/`,
+    []
+  );
+
+  useEffect(
+    () => {
+      async function fetchData() {
+        const result = await axios(getFetchUrl());
+        setData(
+          result.data.message
+        );
+      }
+
+      fetchData();
+    },
+
+    [getFetchUrl]
+  );
+
   return (
     <>
-      <Fantium title="client" />
+      <Fantium title={data} />
       <div />
     </>
   );
--- client/src/app/fantium.tsx
+++ client/src/app/fantium.tsx
@@ -1,9 +1,8 @@
 export function Fantium({ title }: { title: string }) {
   return (
       <div className="bg-slate-900">
-        <h1>
-          <span className="text-white"> Hello there, </span>
-              Welcome {title} 👋
+        <h1 className="text-white">
+              {title} 👋
           </h1>
       </div>
   );
